08300acebfaa3ab9a5b8e6d6ecd3d53ee0f7e1df
aranajhonny <jhonnyjosearana@gmail.com>
2018-03-26 14:06:07 -0400

    update program

--- config.js
+++ config.js
@@ -1,14 +1,11 @@
 const { schema, imports, dependencies, environment, expressions, endpoints } = program;
 
-environment
-  .add('uid', 'A string with the unique deployment ID.')
-
 imports
   .add('zeit')
   .add('twilio')
 
 dependencies
   .add('message', 'twilio:Message')
-  .add('deployments', 'zeit:Root')
+  .add('deployment', 'zeit:Deployment')
 
 schema.type('Root')
--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const { root, deployments, message } = program.refs
-const { uid } = process.env
 
 export async function init() {
 	return program.setTimer('check', 0, 50)
 }
 
 export async function timer({ key }) {
-	const state = await deployments.one({ uid: uid }).query('{ state }')
+	const state = await deployment.query('{ state }')
 	console.log(state)
 }
